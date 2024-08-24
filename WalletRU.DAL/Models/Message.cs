e0bf423fb178a921bfdd5f4f04728066fed3cc6c
Mikhail Popov <astat.popov@yandex.ru>
2024-08-24 03:21:07 +0300

    Initial commit - confirmed application architecture

---
+++ WalletRU.DAL/Models/Message.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WalletRU.DAL.Models;
+
+public sealed class Message
+{
+    public int Id { get; set; }
+    
+    [MaxLength(125)]
+    public string MessageBody { get; set; } = null!;
+
+    public DateTime PublishedAt { get; set; } = DateTime.Now;
+}
---
+++ WalletRU.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace WalletRU.Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
